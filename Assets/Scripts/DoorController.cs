e33adc601a946dd5a5a8e3f7546df4e15be32978
YuHsuanChen <ddy20824@gmail.com>
2025-03-17 18:10:05 -0400

    Add timer in GameUI

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class DoorController : MonoBehaviour
 {
--- Assets/Scripts/GamePlayUIController.cs
+++ Assets/Scripts/GamePlayUIController.cs
@@ -1,8 +1,12 @@
+using TMPro;
 using UnityEngine;
 
 public class GamePlayUIController : MonoBehaviour
 {
+    [SerializeField] private TMP_Text timerText;
     private BarController alertnessBar;
+    float usedTime;
+
     void Awake()
     {
         EventManager.Instance.UpdateAlertnessLevel += UpdateAlertnessLevel;
@@ -21,6 +25,12 @@ public class GamePlayUIController : MonoBehaviour
         alertnessBar.SetValue(0);
         GameState.Instance.SetAlertnessLevel(0);
         EventManager.Instance.TriggerUpdateVision();
+        usedTime = 0;
+    }
+
+    void Update()
+    {
+        UpdateTimer();
     }
 
     private void UpdateAlertnessLevel()
@@ -38,5 +48,13 @@ public class GamePlayUIController : MonoBehaviour
     {
         gameObject.transform.GetChild(0).Find("AlertnessLevel").gameObject.SetActive(false);
         gameObject.transform.GetChild(0).Find("BossHealth").gameObject.SetActive(true);
+    }
+
+    private void UpdateTimer()
+    {
+        usedTime += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(usedTime / 60);
+        int seconds = Mathf.FloorToInt(usedTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
